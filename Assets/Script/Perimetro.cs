328764b9e799dbda44905808ced2782a312b1716
Neisser Sánchez <rko_619_fbi@hotmail.com>
2021-09-03 13:25:33 -0500

    DeV

---
+++ Assets/Script/Perimetro.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour {
+ void OnTriggerExit(Collider other) {
+ Destroy(other.gameObject);
+ }
+}
+
+
+
+
